d8cf3dd2edbe42261e1f49f51ec3658587a62b89
rpmcod3 <137357438+rpmcod3@users.noreply.github.com>
2024-01-14 19:41:26 -0300

    Add files via upload

--- Desafio3/app.js
+++ Desafio3/app.js
@@ -10,6 +10,18 @@ res.send("pong");
 })
 
 
+app.get("/products/:id", (req, res) =>{
+
+
+
+    let products = productManager.getAllProducts()
+
+    res.send(products);
+
+
+})
+
+
 app.listen(3000, () => {
 console.log("Aplicacion funcionando en el puerto 3000")
 
